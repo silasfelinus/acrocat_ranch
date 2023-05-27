ec7f5624401231079f38d97ad8636152db943922
silasfelinus <silasfelinus@gmail.com>
2023-05-27 13:04:13 -0700

    working on https and storyblok

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxt/content'],
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/content',
+    [
+      '@storyblok/nuxt',
+      { accessToken: 'N3DZLOHMxaCqWifnsVQvHgtt-199354-jNL5Lt4q3JvssstLJ35s' }
+    ]
+  ],
   content: {
     documentDriven: true
   }
