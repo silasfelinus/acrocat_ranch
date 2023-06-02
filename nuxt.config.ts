3f9ad47ecd810527f797a18665ba53f6d8eff7fd
silasfelinus <silasfelinus@gmail.com>
2023-06-01 21:10:29 -0700

    removced document driven after errors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
   css: ['~/assets/css/tailwind.css'],
-  content: {
-    documentDriven: true
-  },
 
   devtools: {
     enabled: true
